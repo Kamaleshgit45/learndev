e5af0ad35ab00228e6b6140eb624e7628487e298
Kamaleshgit45 <kamaleshak45@gmail.com>
2024-07-29 16:31:32 +0530

    Create conditional_statement.sh

---
+++ conditional_statement.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#- Write a script that checks if a file exists in the current directory. If it does, print a message confirming its existence; otherwise, print an error message.
+
+echo "please enter the filename: "
+read filename
+
+if [ -e "$filename" ]; then
+	echo "The file '$filename' exists in the current directory"
+else
+	echo "Error: The file '$filename' doe not exsist in the current directory"
+fi
